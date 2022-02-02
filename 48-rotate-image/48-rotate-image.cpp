37a2fc7eac90ab86a993e7144ab7c8bfe33cdd5d
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-02 12:22:47 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (33.49%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -2,15 +2,34 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         
-        reverse(matrix.begin(),matrix.end());
+        // https://leetcode.com/problems/rotate-image/discuss/1449737/Rotation-90Code-180-270-360-explanation-Inplace-O(1)-Space
         
-        for(int i=0;i<matrix.size();i++)
+        //TRANSPOSE (RAR)
+        int n=matrix.size();
+        for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<matrix[0].size();j++)
+            for(int j=i;j<n;j++)
             {
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        
+        
+        for(int i=0;i<n;i++)
+        {
+            int left=0,right=n-1;
+            while(left<right)
+            {
+                swap(matrix[i][left],matrix[i][right]);
+                
+                ++left;
+                --right;    
+            }
+            
+        }
+        
+        
+        
         
     }
 };
