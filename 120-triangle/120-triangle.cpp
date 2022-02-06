a8f31d4b5efc732a20c259190482458a58777138
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-06 10:22:11 +0530

    Time: 15 ms (15.54%), Space: 9.1 MB (9.49%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,20 +1,6 @@
 class Solution {
 public:
     
-    int solve(int row,int col,vector<vector<int>> &triangle,vector<vector<int>> &dp)
-    {
-        if(row==triangle.size()-1)
-            return triangle[row][col];
-        
-        if(dp[row][col]!=-1)
-            return dp[row][col];
-        
-        int down=triangle[row][col]+solve(row+1,col,triangle,dp);
-        int downD=triangle[row][col]+solve(row+1,col+1,triangle,dp);
-        
-        return dp[row][col]=min(down,downD);   
-    }
-    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n=triangle.size();
