dda5ba69b02f9df4e963b2c8ccbf3cd5ca331cf8
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-06 10:21:54 +0530

    Time: 8 ms (64.33%), Space: 9 MB (30.73%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -20,7 +20,22 @@ public:
         int n=triangle.size();
         vector<vector<int>> dp(n,vector<int>(n,-1));
         
-        return solve(0,0,triangle,dp);
+        for(int i=0;i<n;i++)
+            dp[n-1][i]=triangle[n-1][i];
+        
+        for(int i=n-2;i>=0;i--)
+        {
+            int minn=1e9;
+            for(int j=i;j>=0;j--)
+            {
+                int down=triangle[i][j]+dp[i+1][j];
+                int downD=triangle[i][j]+dp[i+1][j+1];
+                
+                dp[i][j]=min(down,downD);
+            }
+        }
+        
+        return dp[0][0];
         
     }
 };
