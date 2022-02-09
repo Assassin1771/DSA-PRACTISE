0364e3638cdde7967cd3d03e08427274abe553c7
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-09 13:19:09 +0530

    Time: 50 ms (91.35%), Space: 22.3 MB (90.63%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,36 +2,67 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
+//         int n=nums.size();
+//         sort(begin(nums),end(nums));
+        
+//         if(n==0)
+//             return 0;
+        
+//         set<int> s;
+//         for(auto i:nums)
+//             s.insert(i);
+        
+//         vector<int> res;
+//         for(auto i:s)
+//             res.push_back(i);
+        
+//         int ans=1,temp=1;
+//         for(int i=1;i<res.size();i++)
+//         {
+//             if(res[i]==res[i-1]+1)
+//                 temp++;
+            
+//             else
+//             {
+//                 ans=max(ans,temp);
+//                 temp=1;
+//             }
+//         }
+        
+//         ans=max(ans,temp);
+        
+//         return ans;
+        
         int n=nums.size();
         sort(begin(nums),end(nums));
         
         if(n==0)
             return 0;
         
-        set<int> s;
-        for(auto i:nums)
-            s.insert(i);
-        
-        vector<int> res;
-        for(auto i:s)
-            res.push_back(i);
-        
-        int ans=1,temp=1;
-        for(int i=1;i<res.size();i++)
+        int res=1,temp=1;
+        for(int i=1;i<n;i++)
         {
-            if(res[i]==res[i-1]+1)
+            if(nums[i]==nums[i-1]+1)
                 temp++;
+            
+            else if(nums[i]==nums[i-1])
+                continue;
             
             else
             {
-                ans=max(ans,temp);
+                res=max(res,temp);
                 temp=1;
             }
         }
         
-        ans=max(ans,temp);
+        res=max(res,temp);
         
-        return ans;
+        return res;
+
+        
+        
+        
+        
         
     }
 };
