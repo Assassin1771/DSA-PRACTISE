d1c28ad4af2142d0679c27e42a9e9ab58848ab08
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-09 13:38:43 +0530

    Time: 166 ms (43.48%), Space: 14.8 MB (89.26%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -5,13 +5,31 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        for(int i=0;i<m;i++)
+//         for(int i=0;i<m;i++)
+//         {
+//             if(binary_search(matrix[i].begin(),matrix[i].end(),target))
+//                 return true;
+//         }
+        
+//         return false;
+        
+        //OR
+        
+        int row=0,col=n-1;
+        while(row<m and col>=0)
         {
-            if(binary_search(matrix[i].begin(),matrix[i].end(),target))
+            if(matrix[row][col]==target)
                 return true;
+            
+            else if(matrix[row][col]>target)
+                col--;
+            
+            else
+                row++;
         }
         
         return false;
+        
         
     }
 };
