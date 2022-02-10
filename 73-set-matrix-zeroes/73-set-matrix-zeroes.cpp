c19bc668b251f51a82a1ef1f7592d5bcb8c53c49
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-02-10 13:00:46 +0530

    Time: 14 ms (72.36%), Space: 13.5 MB (11.10%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     
-    void solve(int x,int y,vector<vector<int>> &temp)
+    void solve1(int x,vector<vector<int>> &temp)
     {
-        for(int i=0;i<temp.size();i++)
-            temp[i][y]=0;
-        
         for(int i=0;i<temp[0].size();i++)
             temp[x][i]=0;
+    }
+    
+    void solve2(int y,vector<vector<int>> &temp)
+    {        
+        for(int i=0;i<temp.size();i++)
+            temp[i][y]=0;
     }
     
     void setZeroes(vector<vector<int>>& matrix) {
         
         // vector<vector<int>> temp=matrix;
-        
-        vector<pair<int,int>> v;
     
         int m=matrix.size();
         int n=matrix[0].size();
+        
+        set<int> s1;
+        set<int> s2;
         
         for(int i=0;i<m;i++)
         {
@@ -25,13 +29,17 @@ public:
             {
                 if(matrix[i][j]==0)
                 {
-                    v.push_back({i,j});
+                    s1.insert(i);
+                    s2.insert(j);
                 }
             }
         }
         
-        for(auto i:v)
-            solve(i.first,i.second,matrix);
+        for(auto i:s1)
+            solve1(i,matrix);
+        
+        for(auto i:s2)
+            solve2(i,matrix);
         
     }
 };
