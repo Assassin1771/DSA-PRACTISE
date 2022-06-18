601b1d7a0814aa7feee03c7387f3678886e9c967
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-06-18 15:49:36 +0530

    Time: 10 ms (50.19%), Space: 9.1 MB (7.74%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     
+    int minSum(int x, int y, vector<vector<int>> &triangle, vector<vector<int>> &dp)
+    {
+        int n=triangle.size();
+        
+        if(x==n-1)
+            return triangle[x][y];
+        
+        if(dp[x][y]!=-1)
+            return dp[x][y];
+        
+        int down=triangle[x][y]+minSum(x+1,y,triangle,dp);
+        int downD=triangle[x][y]+minSum(x+1,y+1,triangle,dp);
+        
+        return dp[x][y]=min(down,downD);
+    }
+    
     int minimumTotal(vector<vector<int>>& triangle) {
         
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
         
-        for(int i=0;i<n;i++)
-            dp[n-1][i]=triangle[n-1][i];
-        
-        for(int i=n-2;i>=0;i--)
-        {
-            int minn=1e9;
-            for(int j=i;j>=0;j--)
-            {
-                int down=triangle[i][j]+dp[i+1][j];
-                int downD=triangle[i][j]+dp[i+1][j+1];
-                
-                dp[i][j]=min(down,downD);
-            }
-        }
-        
-        return dp[0][0];
+        vector<vector<int>> dp(n, vector<int> (n,-1));        
+        return minSum(0,0,triangle,dp);
         
     }
 };
