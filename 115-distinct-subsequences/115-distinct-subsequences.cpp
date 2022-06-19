a16b13e1a4d07bb091bbda4f45a629d0764b88c0
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-06-19 17:33:52 +0530

    Time: 89 ms (15.59%), Space: 30.1 MB (16.08%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -24,9 +24,26 @@ public:
         int m=s.length();
         int n=t.length();
         
-        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        vector<vector<double>> dp(m+1,vector<double> (n+1,0));
         
-        return count(m,n,s,t,dp);
+        for(int i=0;i<=m;i++)
+            dp[i][0]=1;
+        
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j]= dp[i-1][j-1]+dp[i-1][j];
+
+                else
+                    dp[i][j]= dp[i-1][j];
+            }
+        }
+        
+        
+        
+        return dp[m][n];
         
     }
 };
