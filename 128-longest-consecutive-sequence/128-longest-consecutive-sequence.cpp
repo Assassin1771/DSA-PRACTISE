fbe267c4b4c886e3129b6ac0df1f6b01c76d019a
Tushar Garg <58933116+Assassin1771@users.noreply.github.com>
2022-06-26 12:45:15 +0530

    Time: 1124 ms (10.05%), Space: 49.7 MB (54.36%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -2,67 +2,28 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
         
-//         int n=nums.size();
-//         sort(begin(nums),end(nums));
-        
-//         if(n==0)
-//             return 0;
-        
-//         set<int> s;
-//         for(auto i:nums)
-//             s.insert(i);
-        
-//         vector<int> res;
-//         for(auto i:s)
-//             res.push_back(i);
-        
-//         int ans=1,temp=1;
-//         for(int i=1;i<res.size();i++)
-//         {
-//             if(res[i]==res[i-1]+1)
-//                 temp++;
-            
-//             else
-//             {
-//                 ans=max(ans,temp);
-//                 temp=1;
-//             }
-//         }
-        
-//         ans=max(ans,temp);
-        
-//         return ans;
-        
-        int n=nums.size();
-        sort(begin(nums),end(nums));
-        
-        if(n==0)
-            return 0;
-        
-        int res=1,temp=1;
-        for(int i=1;i<n;i++)
-        {
-            if(nums[i]==nums[i-1]+1)
-                temp++;
-            
-            else if(nums[i]==nums[i-1])
-                continue;
-            
-            else
-            {
-                res=max(res,temp);
-                temp=1;
-            }
-        }
-        
-        res=max(res,temp);
-        
-        return res;
+       unordered_set < int > hashSet;
+      for (int num: nums) {
+        hashSet.insert(num);
+      }
 
-        
-        
-        
-        
-        
-    }
+      int longestStreak = 0;
+
+      for (int num: nums) {
+        if (!hashSet.count(num - 1)) {
+          int currentNum = num;
+          int currentStreak = 1;
+
+          while (hashSet.count(currentNum + 1)) {
+            currentNum += 1;
+            currentStreak += 1;
+          }
+
+          longestStreak = max(longestStreak, currentStreak);
+        }
+      }
+
+      return longestStreak;
+
+        }
 };
